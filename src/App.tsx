e5715eda8fb2bb9ac033fafa63a127fe44556bcd
YasDev <88037615+YasDev-coding@users.noreply.github.com>
2025-01-12 18:17:17 +0300

    added theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
 function App() {
   return (
-    <>
-      <h1 className="prose lg:prose-2xl font-serif text-indigo-500">
-        React + Tailwind + Ts
-      </h1>
-      <form action="" className="p-4">
-        <input
-          type="checkbox"
-          name=""
-          id=""
-          className="rounded text-pink-500 focus:outline-red-500"
-        />
-      </form>
-    </>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="p-10"></div>
+    </ThemeProvider>
   );
 }
 
