0e174b43b847825d1b34321ffdcb3b47a0046d78
Yasseen Ar Web Dev <88037615+YasDev-coding@users.noreply.github.com>
2025-01-12 17:34:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# React-ShadcnUI-Ts-Swc-Template
+
+## just run
+
+### install all the libraries
+
+(tailwincss, react-icons, clsx and tailwind plugins)
+`npm install`
+
+Initialize shadcn/UI
+`npx shadcn@latest init`
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <h1 className="prose lg:prose-2xl font-serif text-indigo-500">
+        React + Tailwind + Ts
+      </h1>
+      <form action="" className="p-4">
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          className="rounded text-pink-500 focus:outline-red-500"
+        />
+      </form>
+    </>
+  );
+}
+
+export default App;
